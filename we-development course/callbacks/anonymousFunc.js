772944cb58f33288d29a5f9c6c643396bd79ec77
Devapujyula <sdevapujyula@gmail.com>
2023-08-31 22:04:33 +0530

    added anonymous function in js

---
+++ we-development course/callbacks/anonymousFunc.js
@@ -0,0 +1,7 @@
+const arr = [3, 5, 6, 8]
+
+arr.forEach(function(val) {
+    console.log(val);
+})
+
+// ekkada avuthe function ki name evvakunda direct ga function ni call chesthe dhanni anonymous function ani antaru
