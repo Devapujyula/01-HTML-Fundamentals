88d76efad9c4c87d318849f64adaa3b46cd59a61
Devapujyula <sdevapujyula@gmail.com>
2023-09-13 10:37:37 +0530

    added variables in javascript

---
+++ we-development course/Intro_JS/variables.js
@@ -0,0 +1,17 @@
+let student = "Bobby"
+let score = 100
+let bonus = 20
+let totalScore = score + bonus
+
+
+console.log(student);
+console.log(totalScore);
+
+
+let firstName = "Devapujyula "
+let secondName = "Prakash"
+
+let fullName = firstName + secondName
+
+console.log("The full name of the banker is -",fullName)
+
