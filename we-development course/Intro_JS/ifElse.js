73af595cf4531efc7d52312967b43a4437fa2e1d
Devapujyula <sdevapujyula@gmail.com>
2023-09-16 11:35:18 +0530

    added if-else code in js

---
+++ we-development course/Intro_JS/ifElse.js
@@ -0,0 +1,44 @@
+// const temp = 20
+
+// if (temp <= 32) {
+//     console.log("This is a valid temperature");
+// } else {
+//     console.log("Please enter a valid temperature");
+// }
+
+// if (false) {
+//     console.log("I am inside if Block");
+// } else if (true) {
+//     console.log("I am inside ELSE-IF block");
+// } else {
+//     console.log("I am inside else");
+// }
+
+// LCO
+
+var whoIsHere = 'teacher'
+
+if (whoIsHere === 'user') {
+    console.log("Greetings message for user");
+    console.log("Allow access to use account");
+} else if (whoIsHere === 'teacher') {
+    console.log("Greetings message for Teacher");
+    console.log("Allow access to use account for teacher");
+} else {
+    console.log("MESSAGE VERIFY YOUR ACCOUNT...!");
+    console.log("Send User a email for VERIFICATION...!");
+}
+
+// Assignament
+
+var gradeMarks = 1;
+
+if (gradeMarks = 10) {
+    console.log("You are Amazing...!");
+} else if (gradeMarks = 5) {
+    console.log("You are Good...!");
+} else if (gradeMarks = 3) {
+    console.log("You are poor...!");
+} else {
+    console.log("Better luck next time you falied....!");
+}
