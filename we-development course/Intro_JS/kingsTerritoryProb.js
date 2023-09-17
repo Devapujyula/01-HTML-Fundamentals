68ad7b4c8dbb6324bc5c479fd775487138e2fd95
Devapujyula <sdevapujyula@gmail.com>
2023-09-17 11:29:24 +0530

    added kingsTerritoryProblem code in js

---
+++ we-development course/Intro_JS/kingsTerritoryProb.js
@@ -0,0 +1,16 @@
+// Kings Problem
+
+//let king = "Bobby"
+
+if(true) {
+    let king = 'Kanna'
+
+    if(true) {
+        let king = 'Ram'
+        console.log(king);
+    }
+}
+
+if(true) {
+    console.log(" I am the second part "+king);
+}
