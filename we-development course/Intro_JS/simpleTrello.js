a5e4b0022d4f48c656b6e1c39d4b4fedc52cab45
Devapujyula <sdevapujyula@gmail.com>
2023-09-20 11:44:53 +0530

    added simple trello code in js

---
+++ we-development course/Intro_JS/simpleTrello.js
@@ -0,0 +1,6 @@
+const tasks = ['wake up at 6.00 A.M', 'Excercise complete by 7.30 A.M', 'Complete break fast by 9.00 A.M', 'Work Time upto 6.00 P.M', 'Dinner by 9.00 P.M', 'Go to bed by 10.00 P.M']
+
+tasks.forEach(function(index, task) {
+    console.log(myTodos[index]);
+    console.log(`The Todo's of the day are include ${task+1} is ${index}`);
+})
