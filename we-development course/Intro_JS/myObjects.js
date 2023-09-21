bb5d909fa436c5d50f398929c53c57ce083e7f1c
Devapujyula <sdevapujyula@gmail.com>
2023-09-21 13:31:09 +0530

    code of myObjects in java script

---
+++ we-development course/Intro_JS/myObjects.js
@@ -0,0 +1,8 @@
+let myYoutubeVideo = {
+    title: 'Loops in JavaScript',
+    videoLength: 15,
+    videoCreator: 'Prakash',
+    videoDescription: 'This is a video description'
+}
+
+console.log(`Hey new video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator} `);
