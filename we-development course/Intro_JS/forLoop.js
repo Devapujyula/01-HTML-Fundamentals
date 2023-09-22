a4836f27017dc2bc20abc03e0286d5bd76ff34c0
Devapujyula <sdevapujyula@gmail.com>
2023-09-22 09:27:03 +0530

    code of forLoop in javascript

---
+++ we-development course/Intro_JS/forLoop.js
@@ -0,0 +1,16 @@
+const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+console.log(days);
+console.log(days.length);
+
+for (let index = 0; index < days.length; index++) {
+    // const element = days[index];
+    // console.log(element);
+    console.log(days[index]);
+    
+}
+
+for (let index = days.length - 1; index >= 0; index--) {
+    console.log(days[index]);
+    
+}
