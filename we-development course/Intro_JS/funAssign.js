418c7f40d53dfb907fe29bb7e4ab775088ed198b
Devapujyula <sdevapujyula@gmail.com>
2023-09-22 16:58:59 +0530

    code of funAssign in java script

---
+++ we-development course/Intro_JS/funAssign.js
@@ -0,0 +1,22 @@
+let getMyGrade = function(currentMarks, totalMarks) {
+    let myPercentage = (currentMarks / totalMarks) * 100
+
+    let myGrade = ''
+
+    if (myPercentage >= 90) {
+        myGrade = 'A'
+    } else if (myPercentage >= 80) {
+        myGrade = 'B'
+    }  else if (myPercentage >= 70) {
+        myGrade = 'C'
+    } else if (myPercentage >= 60) {
+        myGrade = 'D'
+    } else {
+        myGrade = 'F'
+    }  
+     
+    return `Your grade is ${myGrade} and percentage is ${myPercentage}`
+}
+
+let yourResult = getMyGrade(92, 150)
+console.log(yourResult);
