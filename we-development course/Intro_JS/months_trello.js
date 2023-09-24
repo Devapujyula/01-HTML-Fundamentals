cb3561a38a72cdf70bebead07e415a9d3532d8e4
Devapujyula <sdevapujyula@gmail.com>
2023-09-24 22:13:12 +0530

    code of months trello in java script

---
+++ we-development course/Intro_JS/months_trello.js
@@ -0,0 +1,7 @@
+// Assignment-1
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+months.forEach(function(month, index) {
+    console.log(`The ${month} month is having the index of ${index+1}`);
+})
