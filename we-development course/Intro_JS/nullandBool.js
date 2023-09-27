13741a0caafc291726c70193c070e3fe350a1092
Devapujyula <sdevapujyula@gmail.com>
2023-09-27 10:43:45 +0530

    added null and boolean in js

---
+++ we-development course/Intro_JS/nullandBool.js
@@ -0,0 +1,25 @@
+//temp is coming from website API
+
+var temp = 32 
+
+// done some calculation
+// temp = 3
+
+console.log("Current Temperature is: " + temp);
+
+//Grade system for a school
+
+// 10 - Great Job
+// 5 - work harder
+// 2 - poor
+// 0 - fail
+
+// Boolean - True/False
+
+let grade = true
+
+let actualMarks = 7 
+
+let myGrade = (actualMarks == 10)
+
+console.log(myGrade);
