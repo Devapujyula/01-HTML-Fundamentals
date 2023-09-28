0a163073a1fe716dc35eee7639900d85d2bc8a67
Devapujyula <sdevapujyula@gmail.com>
2023-09-28 15:33:35 +0530

    added objects assignment code

---
+++ we-development course/Intro_JS/objAssign.js
@@ -0,0 +1,8 @@
+let javascriptCourse = {
+    courseName : 'An Intorduction to JavaScript',
+    courseDescription : 'This is JavaScript introduction course for beginners',
+    courseAuthor: 'Prakash Dsp',
+    coursePrice : 400,
+}
+
+console.log(`Hey there is a new course ${javascriptCourse.courseName} by ${javascriptCourse.courseAuthor} and with ${javascriptCourse.courseDescription} with price ${javascriptCourse.coursePrice}`);
