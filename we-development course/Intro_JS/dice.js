e018a633e4c51381fce6950b5477c9311b58af87
Devapujyula <sdevapujyula@gmail.com>
2023-10-01 18:32:17 +0530

    added dice in js code

---
+++ we-development course/Intro_JS/dice.js
@@ -0,0 +1,19 @@
+let numOne = 22
+let numTwo = 7
+
+result = 22/7
+
+// console.log(result.toFixed(2));
+// let flotRes = result.toFixed(2)
+
+// console.log(Math.floor(flotRes));
+// console.log(Math.ceil(flotRes));
+
+// console.log(Math.random() * 5 + 1);
+
+let upper = 6
+let lower = 1
+
+let myRandon = Math.floor(Math.random() * (upper - lower + 1)) + lower
+
+console.log(myRandon);
