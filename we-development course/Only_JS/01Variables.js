31f8901aae113c09073448d5ef12a073a2cdee28
Devapujyula <sdevapujyula@gmail.com>
2023-10-03 11:44:06 +0530

    added variables code in js

---
+++ we-development course/Only_JS/01Variables.js
@@ -0,0 +1,21 @@
+var fullName = 'Prakash Devapujyula'
+
+var courseName = 'Web Development'
+
+var isLoggedIn = false
+
+var loggedCount = 34
+
+var paymentMode;
+
+console.log(paymentMode);
+
+paymentMode = 'Credit Card'
+
+paymentMode = ['Credit Card', 'Debit Card', 'UPI']
+
+console.log(fullName);
+console.log(courseName);
+console.log(isLoggedIn);
+console.log(loggedCount);
+console.log(paymentMode);
