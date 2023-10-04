50beafaa806654126e0952ffc10c08fb1324bbd1
Devapujyula <sdevapujyula@gmail.com>
2023-10-04 12:19:55 +0530

    added varibles form in js

---
+++ we-development course/Only_JS/02Variables-Form.js
@@ -0,0 +1,31 @@
+const userId = 'abc123'
+
+uId = 'abcd234'
+
+var fullName = 'Prakash Devapujyula'
+var emailId = 'prakashdsp7777#gmail.com'
+var password = '123456'
+var confirmPassword = '123456'
+var courseCount = 0
+var isLoggedInFromGoogle = false
+
+// fullname = prompt('Enter Your name')
+
+console.log("Full Name is:", fullName);
+console.log("Email Id is:",emailId);
+console.log(password);
+console.log(confirmPassword);
+console.log(courseCount);
+console.log(isLoggedInFromGoogle);
+
+console.log(`
+With unique Id: ${uid}
+User is: ${fullName}
+and his email is: ${emailId}
+and uses the password: ${password}
+and his course count: ${courseCount}
+and wheather is he logged in : ${isLoggedInFromGoogle}
+
+`);
+
+console.log(userId);
