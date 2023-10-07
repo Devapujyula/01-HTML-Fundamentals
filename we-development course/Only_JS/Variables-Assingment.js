e3744aa5366718c29f81598b51f1d49f641ad635
Devapujyula <sdevapujyula@gmail.com>
2023-10-07 11:03:57 +0530

    added variables assingment code in js

---
+++ we-development course/Only_JS/Variables-Assingment.js
@@ -0,0 +1,14 @@
+const firstName = 'Prakash'
+const lastName = 'Devapujyula'
+const emailId = 'sdevapujyula@gmail.com'
+const countryName = 'Bharat'
+const stateName = 'AndhraPradesh'
+const password = '123456'
+const countCourse = 0
+const isLoggedInFromGoogle = true
+
+console.log(`
+This is ${firstName}, ${lastName} with an Email-${emailId} in the country: ${countryName} at state of ${stateName} having password ${password} with course count: ${countCourse} and whether user is logged in are not with status: ${isLoggedInFromGoogle}
+
+
+`);
