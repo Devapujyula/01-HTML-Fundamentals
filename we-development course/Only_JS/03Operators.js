58c393d80d3f6b2cc82db35b33dce73528794d57
Devapujyula <sdevapujyula@gmail.com>
2023-10-09 13:25:32 +0530

    added operators in js

---
+++ we-development course/Only_JS/03Operators.js
@@ -0,0 +1,30 @@
+// variable name cannot start with number
+
+// var num1 = 7
+// var num2 = 12
+
+// console.log(num1 + num2);
+
+// var answer = num1 > num2
+
+// console.log(answer);
+
+// Discount = ((ListPrice - SellingPrice) / ListPrice) * 100
+
+
+var sellingPrice = 199
+
+var listPrice = 799
+
+var discountPercent = ((listPrice - sellingPrice) / listPrice) * 100
+
+console.log("The Discount Percentage is:",discountPercent);
+
+displayDiscountPercentage = Math.round(discountPercent)
+
+console.log(displayDiscountPercentage + "% off");
+
+
+var result = listPrice > sellingPrice
+
+console.log(typeof result);
