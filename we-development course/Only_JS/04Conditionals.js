f9c47f5aa9c982faa4339f5f6a59950b02af7361
Devapujyula <sdevapujyula@gmail.com>
2023-10-10 13:05:49 +0530

    added conditionals in js

---
+++ we-development course/Only_JS/04Conditionals.js
@@ -0,0 +1,21 @@
+var temperature;
+
+// TODO: GO to google and get the temperature data
+
+temperature = 39;
+
+// if(true) {
+//     console.log("It's very cold temperature");
+// }
+
+if (temperature < 20) {
+    console.log("It's very Cold outside");
+} 
+
+if(temperature < 30) {
+    console.log("It's Moderate outside");
+}
+
+else {
+    console.log("It's really Hot outside");
+}
