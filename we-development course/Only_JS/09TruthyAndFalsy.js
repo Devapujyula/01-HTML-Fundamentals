c672ea865244c166339c2e00837061ee631ef326
Devapujyula <sdevapujyula@gmail.com>
2023-10-15 17:23:45 +0530

    added turth and falsy statements in js

---
+++ we-development course/Only_JS/09TruthyAndFalsy.js
@@ -0,0 +1,20 @@
+// These all are falsy statements 
+//undefined
+// null
+// 0
+// ''
+// NaN
+
+var user;
+
+var user1 = ''
+
+console.log(user);
+console.log(user1);
+
+var user = "";
+if (user) {
+    console.log("Condition is true");
+}
+
+console.log(2 + "2");
