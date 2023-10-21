4bcdbbac7a3d3c7f11ee85467670c77fb61e5cb6
Devapujyula <sdevapujyula@gmail.com>
2023-10-21 08:21:56 +0530

    added jsContext code in js

---
+++ we-development course/Only_JS/12jsContext.js
@@ -0,0 +1,25 @@
+function sayHello() {
+    console.log("Hello");
+}
+
+sayHello();
+
+greetings()
+
+function greetings() {
+    console.log("Greetings");
+}
+
+if (2 === "2") {
+    console.log("This is true");
+}
+
+var myName = "Prakash"
+
+myName
+
+
+// This window word works only on browser
+if(myName === window.myName) {
+    console.log("A true statement");
+}
